ad98dba3d86079d0a213e8f3e3caa728b5c64ce1
Mike Greenberg <mikeg@ob1.io>
2018-02-14 18:43:00 -0500

    [#816] Add ModeratorIDs to listing.json

--- repo/migrations/Migration006.go
+++ repo/migrations/Migration006.go
@@ -1,104 +1,199 @@
 package migrations
 
 import (
+	"database/sql"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
+
+	"github.com/phoreproject/openbazaar-go/util"
 )
 
-var Migration006 migration006
+type migration006_configRecord struct {
+	StoreModerators []string `json:"storeModerators"`
+}
+
+type migration006_price struct {
+	CurrencyCode string `json:"currencyCode"`
+	Amount       uint64 `json:"amount"`
+}
+type migration006_thumbnail struct {
+	Tiny   string `json:"tiny"`
+	Small  string `json:"small"`
+	Medium string `json:"medium"`
+}
+
+type migration006_listingDataBeforeMigration struct {
+	Hash          string                 `json:"hash"`
+	Slug          string                 `json:"slug"`
+	Title         string                 `json:"title"`
+	Categories    []string               `json:"categories"`
+	NSFW          bool                   `json:"nsfw"`
+	ContractType  string                 `json:"contractType"`
+	Description   string                 `json:"description"`
+	Thumbnail     migration006_thumbnail `json:"thumbnail"`
+	Price         migration006_price     `json:"price"`
+	ShipsTo       []string               `json:"shipsTo"`
+	FreeShipping  []string               `json:"freeShipping"`
+	Language      string                 `json:"language"`
+	AverageRating float32                `json:"averageRating"`
+	RatingCount   uint32                 `json:"ratingCount"`
+}
+
+type migration006_listingDataAfterMigration struct {
+	Hash          string                 `json:"hash"`
+	Slug          string                 `json:"slug"`
+	Title         string                 `json:"title"`
+	Categories    []string               `json:"categories"`
+	NSFW          bool                   `json:"nsfw"`
+	ContractType  string                 `json:"contractType"`
+	Description   string                 `json:"description"`
+	Thumbnail     migration006_thumbnail `json:"thumbnail"`
+	Price         migration006_price     `json:"price"`
+	ShipsTo       []string               `json:"shipsTo"`
+	FreeShipping  []string               `json:"freeShipping"`
+	Language      string                 `json:"language"`
+	AverageRating float32                `json:"averageRating"`
+	RatingCount   uint32                 `json:"ratingCount"`
+	ModeratorIDs  []string               `json:"moderatorIDs"`
+}
 
 type migration006 struct{}
 
-func (migration006) Up(repoPath string, dbPassword string, testnet bool) error {
-	configFile, err := ioutil.ReadFile(path.Join(repoPath, "config"))
+func (migration006) Up(repoPath, databasePassword string, testnetEnabled bool) error {
+	paths, err := util.NewCustomSchemaManager(util.SchemaContext{
+		RootPath:        repoPath,
+		TestModeEnabled: testnetEnabled,
+	})
+
+	// Get ModeratorIDs
+	db, err := sql.Open("sqlite3", paths.DatastorePath())
 	if err != nil {
 		return err
 	}
-	var cfgIface interface{}
-	json.Unmarshal(configFile, &cfgIface)
-	cfg, ok := cfgIface.(map[string]interface{})
-	if !ok {
-		return errors.New("Invalid config file")
+	if databasePassword != "" {
+		p := fmt.Sprintf("PRAGMA key = '%s';", databasePassword)
+		_, err := db.Exec(p)
+		if err != nil {
+			return err
+		}
 	}
-	ipns, ok := cfg["Ipns"]
-	if !ok {
-		return errors.New("Ipns config not found")
+	var (
+		configJSON   []byte
+		configRecord migration006_configRecord
+	)
+	configQuery := db.QueryRow("SELECT value FROM config WHERE key = 'settings' LIMIT 1")
+	if err = configQuery.Scan(&configJSON); err != nil {
+		return err
 	}
-	ipnsCfg, ok := ipns.(map[string]interface{})
-	if !ok {
-		return errors.New("Ipns config not found")
+	if err = json.Unmarshal(configJSON, &configRecord); err != nil {
+		return err
 	}
-	ipnsCfg["QuerySize"] = 1
-	ipnsCfg["BackUpAPI"] = "https://gateway.ob1.io/ob/ipns/"
-	out, err := json.MarshalIndent(cfg, "", "   ")
+
+	// Listing transformation
+	var (
+		listingJSON     []byte
+		listingRecords  []migration006_listingDataBeforeMigration
+		migratedRecords []migration006_listingDataAfterMigration
+	)
+	listingJSON, err = ioutil.ReadFile(paths.RootPathJoin("listings.json"))
 	if err != nil {
 		return err
 	}
-	f, err := os.Create(path.Join(repoPath, "config"))
+	if err = json.Unmarshal(listingJSON, &listingRecords); err != nil {
+		return err
+	}
+
+	for _, listing := range listingRecords {
+		migratedRecords = append(migratedRecords, migration006_listingDataAfterMigration{
+			Hash:          listing.Hash,
+			Slug:          listing.Slug,
+			Title:         listing.Title,
+			Categories:    listing.Categories,
+			NSFW:          listing.NSFW,
+			ContractType:  listing.ContractType,
+			Description:   listing.Description,
+			Thumbnail:     listing.Thumbnail,
+			Price:         listing.Price,
+			ShipsTo:       listing.ShipsTo,
+			FreeShipping:  listing.FreeShipping,
+			Language:      listing.Language,
+			AverageRating: listing.AverageRating,
+			RatingCount:   listing.RatingCount,
+			ModeratorIDs:  configRecord.StoreModerators,
+		})
+	}
+	if listingJSON, err = json.Marshal(migratedRecords); err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(paths.RootPathJoin("listings.json"), listingJSON, os.ModePerm)
 	if err != nil {
 		return err
 	}
-	_, err = f.Write(out)
+
+	// Bump schema version
+	err = ioutil.WriteFile(paths.RootPathJoin("repover"), []byte("7"), os.ModePerm)
 	if err != nil {
 		return err
 	}
-	f.Close()
-	f1, err := os.Create(path.Join(repoPath, "repover"))
-	if err != nil {
-		return err
-	}
-	_, err = f1.Write([]byte("6"))
-	if err != nil {
-		return err
-	}
-	f1.Close()
 	return nil
 }
-func (migration006) Down(repoPath string, dbPassword string, testnet bool) error {
-	configFile, err := ioutil.ReadFile(path.Join(repoPath, "config"))
+
+func (migration006) Down(repoPath, databasePassword string, testnetEnabled bool) error {
+	paths, err := util.NewCustomSchemaManager(util.SchemaContext{
+		RootPath:        repoPath,
+		TestModeEnabled: testnetEnabled,
+	})
 	if err != nil {
 		return err
 	}
-	var cfgIface interface{}
-	json.Unmarshal(configFile, &cfgIface)
-	cfg, ok := cfgIface.(map[string]interface{})
-	if !ok {
-		return errors.New("Invalid config file")
-	}
-	ipns, ok := cfg["Ipns"]
-	if !ok {
-		return errors.New("Ipns config not found")
-	}
-	ipnsCfg, ok := ipns.(map[string]interface{})
-	if !ok {
-		return errors.New("Ipns config not found")
-	}
-	ipnsCfg["QuerySize"] = 5
-	ipnsCfg["BackUpAPI"] = ""
-	out, err := json.MarshalIndent(cfg, "", "   ")
+
+	// Listing transformation
+	var (
+		listingJSON     []byte
+		listingRecords  []migration006_listingDataAfterMigration
+		migratedRecords []migration006_listingDataBeforeMigration
+	)
+	listingJSON, err = ioutil.ReadFile(paths.RootPathJoin("listings.json"))
 	if err != nil {
 		return err
 	}
-	f, err := os.Create(path.Join(repoPath, "config"))
+	if err = json.Unmarshal(listingJSON, &listingRecords); err != nil {
+		return err
+	}
+
+	for _, listing := range listingRecords {
+		migratedRecords = append(migratedRecords, migration006_listingDataBeforeMigration{
+			Hash:          listing.Hash,
+			Slug:          listing.Slug,
+			Title:         listing.Title,
+			Categories:    listing.Categories,
+			NSFW:          listing.NSFW,
+			ContractType:  listing.ContractType,
+			Description:   listing.Description,
+			Thumbnail:     listing.Thumbnail,
+			Price:         listing.Price,
+			ShipsTo:       listing.ShipsTo,
+			FreeShipping:  listing.FreeShipping,
+			Language:      listing.Language,
+			AverageRating: listing.AverageRating,
+			RatingCount:   listing.RatingCount,
+		})
+	}
+	if listingJSON, err = json.Marshal(migratedRecords); err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(paths.RootPathJoin("listings.json"), listingJSON, os.ModePerm)
 	if err != nil {
 		return err
 	}
-	_, err = f.Write(out)
+
+	// Revert schema version
+	err = ioutil.WriteFile(paths.RootPathJoin("repover"), []byte("6"), os.ModePerm)
 	if err != nil {
 		return err
 	}
-	f.Close()
-	f1, err := os.Create(path.Join(repoPath, "repover"))
-	if err != nil {
-		return err
-	}
-	_, err = f1.Write([]byte("5"))
-	if err != nil {
-		return err
-	}
-	f1.Close()
+
 	return nil
 }
--- repo/migrations/Migration006_test.go
+++ repo/migrations/Migration006_test.go
@@ -1,74 +1,155 @@
 package migrations
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/phoreproject/openbazaar-go/util"
 )
 
-var testConfig5 string = `{
-    "Ipns": {
-            "QuerySize": 5,
-            "RecordLifetime": "7d",
-            "RepublishPeriod": "24h",
-            "ResolveCacheSize": 128,
-            "UsePersistentCache": true
-    }
-}`
-
 func TestMigration006(t *testing.T) {
-	f, err := os.Create("./config")
+
+	// Setup
+	basePath := util.GenerateTempPath()
+	testRepoPath, err := util.OpenbazaarPathTransform(basePath, true)
 	if err != nil {
+		t.Fatal(err)
+	}
+	paths, err := util.NewCustomSchemaManager(util.SchemaContext{RootPath: testRepoPath, TestModeEnabled: true})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = os.MkdirAll(paths.RootPathJoin("datastore"), os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	defer paths.DestroySchemaDirectories()
+	var (
+		databasePath = paths.DatastorePath()
+		listingPath  = paths.RootPathJoin("listings.json")
+		schemaPath   = paths.RootPathJoin("repover")
+
+		schemaSql               = "PRAGMA key = 'foobarbaz';"
+		configCreateSql         = "CREATE TABLE `config` (`key` text NOT NULL, `value` blob, PRIMARY KEY(`key`));"
+		expectedStoreModerators = []string{
+			"storemoderator_peerid_1",
+			"storemoderator_peerid_2",
+		}
+		listingFixtures = []migration006_listingDataBeforeMigration{
+			migration006_listingDataBeforeMigration{Hash: "Listing1"},
+			migration006_listingDataBeforeMigration{Hash: "Listing2"},
+		}
+		configRecord         = migration006_configRecord{StoreModerators: expectedStoreModerators}
+		insertConfigTemplate = "INSERT INTO config (key,value) VALUES ('settings','%s');"
+	)
+
+	// Create/persist settings record
+	configJson, err := json.Marshal(configRecord)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dbSetupSql := strings.Join([]string{
+		schemaSql,
+		configCreateSql,
+		fmt.Sprintf(insertConfigTemplate, configJson),
+	}, " ")
+	db, err := sql.Open("sqlite3", databasePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = db.Exec(dbSetupSql)
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.Close()
+
+	// Create listings.json file
+	listingJSON, err := json.Marshal(listingFixtures)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = ioutil.WriteFile(listingPath, listingJSON, os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create schema version file
+	if err = ioutil.WriteFile(schemaPath, []byte("6"), os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+
+	// Execute Migration Up
+	migration := migration006{}
+	if err := migration.Up(testRepoPath, "foobarbaz", true); err != nil {
+		t.Fatal(err)
+	}
+
+	// Migration Up Assertions
+	if err = paths.MustVerifySchemaVersion("7"); err != nil {
+		t.Fatal(err)
+	}
+
+	listingJSON, err = ioutil.ReadFile(listingPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var migratedListings = make([]migration006_listingDataAfterMigration, 2)
+	err = json.Unmarshal(listingJSON, &migratedListings)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if actualCount := len(migratedListings); actualCount != len(listingFixtures) {
+		t.Fatalf("Unexpected number of listings returned. Actual count: %d", actualCount)
+	}
+
+	for _, listing := range migratedListings {
+		actualModeratorIDs := make([]string, 0)
+		for _, peerID := range listing.ModeratorIDs {
+			actualModeratorIDs = append(actualModeratorIDs, peerID)
+		}
+
+		if !reflect.DeepEqual(expectedStoreModerators, actualModeratorIDs) {
+			t.Fatalf("Expected moderator IDs were not equal\n\tExpected: %+v\n\tActual: %+v",
+				expectedStoreModerators,
+				actualModeratorIDs,
+			)
+		}
+	}
+
+	// Execute Migration Down
+	if err = migration.Down(testRepoPath, "foobarbaz", true); err != nil {
+		t.Fatal(err)
+	}
+
+	// Migration Down Assertions
+	if err = paths.MustVerifySchemaVersion("6"); err != nil {
 		t.Error(err)
 	}
-	f.Write([]byte(testConfig5))
-	f.Close()
-	var m migration005
-	// Up
-	err = m.Up("./", "", false)
+
+	listingJSON, err = ioutil.ReadFile(listingPath)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-	newConfig, err := ioutil.ReadFile("./config")
+
+	migratedListings = make([]migration006_listingDataAfterMigration, 2)
+	err = json.Unmarshal(listingJSON, &migratedListings)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-	if !strings.Contains(string(newConfig), `"QuerySize": 1`) {
-		t.Error("Failed to write new QuerySize")
+
+	if actualCount := len(migratedListings); actualCount != len(listingFixtures) {
+		t.Fatalf("Unexpected number of listings returned. Actual count: %d", actualCount)
 	}
-	if !strings.Contains(string(newConfig), `"BackUpAPI": "https://gateway.ob1.io/ob/ipns/"`) {
-		t.Error("Failed to write new BackUpAPI")
+
+	for _, listing := range migratedListings {
+		if len(listing.ModeratorIDs) != 0 {
+			t.Fatalf("Unexpected ModeratorIDs values: %+v", listing.ModeratorIDs)
+		}
 	}
-	repoVer, err := ioutil.ReadFile("./repover")
-	if err != nil {
-		t.Error(err)
-	}
-	if string(repoVer) != "6" {
-		t.Error("Failed to write new repo version")
-	}
-	// Down
-	err = m.Down("./", "", false)
-	if err != nil {
-		t.Error(err)
-	}
-	newConfig, err = ioutil.ReadFile("./config")
-	if err != nil {
-		t.Error(err)
-	}
-	if !strings.Contains(string(newConfig), `"QuerySize": 5`) {
-		t.Error("Failed to write new QuerySize")
-	}
-	if !strings.Contains(string(newConfig), `"BackUpAPI": ""`) {
-		t.Error("Failed to write new BackUpAPI")
-	}
-	repoVer, err = ioutil.ReadFile("./repover")
-	if err != nil {
-		t.Error(err)
-	}
-	if string(repoVer) != "5" {
-		t.Error("Failed to write new repo version")
-	}
-	os.Remove("./config")
-	os.Remove("./repover")
 }
