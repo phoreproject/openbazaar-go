9b2730c2b35e87c0b9423ab58a92b8a327fe44b6
Tyler Smith <tylersmith.me@gmail.com>
2018-04-24 23:28:33 -0700

    BUGFIX: Run fulfilment validations.

--- core/fulfillment.go
+++ core/fulfillment.go
@@ -104,7 +104,10 @@ func (n *OpenBazaarNode) FulfillOrder(fulfillment *pb.OrderFulfillment, contract
 	}
 
 	if listing.Metadata.ContractType == pb.Listing_Metadata_CRYPTOCURRENCY {
-		validateCryptocurrencyFulfillment(fulfillment)
+		err := validateCryptocurrencyFulfillment(fulfillment)
+		if err != nil {
+			return err
+		}
 	}
 
 	ts, err := ptypes.TimestampProto(time.Now())
