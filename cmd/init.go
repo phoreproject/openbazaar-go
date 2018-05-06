b2ce92038f6e04182ecb25563a2313f56d948d52
Vadim <chuvak117@gmail.com>
2018-05-06 21:02:15 +0300

    Fix golint errors

--- cmd/init.go
+++ cmd/init.go
@@ -14,6 +14,7 @@ import (
 
 var log = logging.MustGetLogger("cmd")
 
+//Init struct
 type Init struct {
 	Password           string `short:"p" long:"password" description:"the encryption password if the database is to be encrypted"`
 	DataDir            string `short:"d" long:"datadir" description:"specify the data directory to be used"`
@@ -23,6 +24,7 @@ type Init struct {
 	WalletCreationDate string `short:"w" long:"walletcreationdate" description:"specify the date the seed was created. if omitted the wallet will sync from the oldest checkpoint."`
 }
 
+//Execute init command
 func (x *Init) Execute(args []string) error {
 	// Set repo path
 	repoPath, err := repo.GetRepoPath(x.Testnet)
@@ -56,9 +58,8 @@ func (x *Init) Execute(args []string) error {
 			}
 			fmt.Printf("OpenBazaar repo initialized at %s\n", repoPath)
 			return nil
-		} else {
-			return nil
 		}
+		return nil
 	} else if err != nil {
 		return err
 	}
--- cmd/start.go
+++ cmd/start.go
@@ -87,10 +87,12 @@ var fileLogFormat = logging.MustStringFormatter(
 	`%{time:15:04:05.000} [%{shortfunc}] [%{level}] %{message}`,
 )
 
+//ErrNoGateways
 var (
 	ErrNoGateways = errors.New("No gateway addresses configured")
 )
 
+//Start struct
 type Start struct {
 	Password             string   `short:"p" long:"password" description:"the encryption password if the database is encrypted"`
 	LogLevel             string   `short:"l" long:"loglevel" description:"set the logging level [debug, info, notice, warning, error, critical]" defaut:"debug"`
@@ -111,6 +113,7 @@ type Start struct {
 	Storage              string   `long:"storage" description:"set the outgoing message storage option [self-hosted, dropbox] default=self-hosted"`
 }
 
+//Execute start command
 func (x *Start) Execute(args []string) error {
 	printSplashScreen(x.Verbose)
 
@@ -532,7 +535,7 @@ func (x *Start) Execute(args []string) error {
 			f.Close()
 		} else {
 			if string(cookie)[:len(cookiePrefix)] != cookiePrefix {
-				return errors.New("Invalid authentication cookie. Delete it to generate a new one.")
+				return errors.New("Invalid authentication cookie. Delete it to generate a new one")
 			}
 			split := strings.SplitAfter(string(cookie), cookiePrefix)
 			authCookie.Value = split[1]
@@ -712,25 +715,30 @@ func printSwarmAddrs(node *ipfscore.IpfsNode) {
 	}
 }
 
+//DummyWriter struct
 type DummyWriter struct{}
 
 func (d *DummyWriter) Write(p []byte) (n int, err error) {
 	return 0, nil
 }
 
+//DummyListener struct
 type DummyListener struct {
 	addr net.Addr
 }
 
+//Addr return addr
 func (d *DummyListener) Addr() net.Addr {
 	return d.addr
 }
 
+//Accept retunr conn
 func (d *DummyListener) Accept() (net.Conn, error) {
 	conn, _ := net.FileConn(nil)
 	return conn, nil
 }
 
+//Close return nil
 func (d *DummyListener) Close() error {
 	return nil
 }
@@ -805,6 +813,7 @@ func newHTTPGateway(node *core.OpenBazaarNode, authCookie http.Cookie, config re
 	return api.NewGateway(node, authCookie, gwLis.NetListener(), config, ml, opts...)
 }
 
+//DHTOption variable
 var DHTOption ipfscore.RoutingOption = constructDHTRouting
 
 func constructDHTRouting(ctx context.Context, host p2phost.Host, dstore ipfsrepo.Datastore) (routing.IpfsRouting, error) {
@@ -876,6 +885,7 @@ func serveHTTPApi(cctx *commands.Context) (<-chan error, error) {
 	return errc, nil
 }
 
+//InitializeRepo initialize repo
 func InitializeRepo(dataDir, password, mnemonic string, testnet bool, creationDate time.Time) (*db.SQLiteDatastore, error) {
 	// Database
 	sqliteDB, err := db.Create(dataDir, password, testnet)
