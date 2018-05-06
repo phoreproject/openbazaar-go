73cdb8393717dadc77e7490289407d3afbcd4754
Vadim <chuvak117@gmail.com>
2018-05-06 15:20:19 +0300

    Added gencerts

--- openbazaard.go
+++ openbazaard.go
@@ -20,9 +20,11 @@ type Opts struct {
 }
 type Stop struct{}
 type Restart struct{}
+type Gencerts struct{}
 
 var stopServer Stop
 var restartServer Restart
+var generateCertificates Gencerts
 
 var opts Opts
 
@@ -51,6 +53,10 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+	parser.AddCommand("gencerts",
+		"Generate certificates",
+		"Generate self-singned certificates",
+		&generateCertificates)
 	parser.AddCommand("init",
 		"initialize a new repo and exit",
 		"Initializes a new repo without starting the server",
