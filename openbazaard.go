d4b626816a0f64197ade86c611a405ce77e95206
Vadim <chuvak117@gmail.com>
2018-05-06 19:49:51 +0300

    v.3

--- openbazaard.go
+++ openbazaard.go
@@ -20,11 +20,9 @@ type Opts struct {
 }
 type Stop struct{}
 type Restart struct{}
-type Gencerts struct{}
 
 var stopServer Stop
 var restartServer Restart
-var generateCertificates Gencerts
 
 var opts Opts
 
@@ -53,10 +51,11 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+
 	parser.AddCommand("gencerts",
 		"Generate certificates",
 		"Generate self-singned certificates",
-		&generateCertificates)
+		&cmd.GenerateCertificates{})
 	parser.AddCommand("init",
 		"initialize a new repo and exit",
 		"Initializes a new repo without starting the server",
