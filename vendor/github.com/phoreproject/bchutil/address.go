4bdee5d0eaac3dc8d25d75da6a6ac674b1c9abd1
anchaj <anchaj@users.noreply.github.com>
2018-09-15 11:52:44 +0200

    update deps

---
+++ vendor/github.com/phoreproject/bchutil/address.go
@@ -0,0 +1,25 @@
+package bchutil
+
+import (
+	"github.com/phoreproject/btcd/txscript"
+	"github.com/phoreproject/btcutil"
+	"errors"
+)
+
+func PayToAddrScript(addr btcutil.Address) ([]byte, error) {
+	var script []byte
+	var err error
+	script, err = txscript.PayToAddrScript(addr)
+	if err == nil {
+		return script, nil
+	}
+	script, err = cashPayToAddrScript(addr)
+	if err == nil {
+		return script, nil
+	}
+	script, err = bitpayPayToAddrScript(addr)
+	if err == nil {
+		return script, nil
+	}
+	return script, errors.New("Unrecognized address format")
+}
