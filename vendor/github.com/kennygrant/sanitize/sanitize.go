fa4d778c177e35551636e602c177feeb3cd8c517
anchaj <anchaj@users.noreply.github.com>
2018-09-17 19:09:06 +0200

    fix bad merge in sanitize.go

--- vendor/github.com/kennygrant/sanitize/sanitize.go
+++ vendor/github.com/kennygrant/sanitize/sanitize.go
@@ -16,11 +16,7 @@ import (
 var (
 	ignoreTags = []string{"title", "script", "style", "iframe", "frame", "frameset", "noframes", "noembed", "embed", "applet", "object", "base"}
 
-<<<<<<< HEAD
-	defaultTags = []string{"h1", "h2", "h3", "h4", "h5", "h6", "div", "span", "hr", "p", "br", "b", "i", "strong", "em", "ol", "ul", "li", "a", "img", "pre", "code", "blockquote"}
-=======
 	defaultTags = []string{"h1", "h2", "h3", "h4", "h5", "h6", "div", "span", "hr", "p", "br", "b", "i", "strong", "em", "ol", "ul", "li", "a", "img", "pre", "code", "blockquote", "article", "section"}
->>>>>>> 159c03b9... IPFS rebase second pass
 
 	defaultAttributes = []string{"id", "class", "src", "href", "title", "alt", "name", "rel"}
 )
