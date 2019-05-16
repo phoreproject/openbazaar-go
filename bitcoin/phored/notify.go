3fef7e7765c95adbd2c3a223ea8e1e1b72006804
anchaj <anchaj@users.noreply.github.com>
2019-05-16 19:55:58 +0200

    Add cookies jar to websocket connection for AWL ALB sticky session support.

--- bitcoin/phored/notify.go
+++ bitcoin/phored/notify.go
@@ -5,7 +5,9 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -37,9 +39,12 @@ func (n *NotificationListener) updateFilterAndSend() {
 
 	toSend := []byte(fmt.Sprintf("subscribeBloom %s %d %d 0", hex.EncodeToString(message.Filter), message.HashFuncs, message.Tweak))
 
-	//log.Debugf("<- %s", toSend)
+	//log.Debugf("<- toSend %s", toSend)
 
-	n.conn.WriteMessage(websocket.TextMessage, toSend)
+	err = n.conn.WriteMessage(websocket.TextMessage, toSend)
+	if err != nil{
+		log.Errorf("Bloom filter subscription failed %s", err)
+	}
 }
 
 func startNotificationListener(wallet *RPCWallet) (*NotificationListener, error) {
@@ -47,21 +52,25 @@ func startNotificationListener(wallet *RPCWallet) (*NotificationListener, error)
 	u := url.URL{Scheme: "wss", Host: wallet.rpcBasePath, Path: "/ws"}
 
 	log.Infof("Connecting to %s", u.String())
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	log.Infof("Connected to websockets!")
 
+	dialerWithCookies := websocket.DefaultDialer
+	dialerWithCookies.Jar = *new(http.CookieJar)
+	conn, _, err := dialerWithCookies.Dial(u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
+	log.Info("Connected to websockets!")
 
 	notificationListener.conn = conn
 
-	ticker := time.NewTicker(10 * time.Second)
-
+	ticker := time.NewTicker(15 * time.Second)
 	go func() {
 		for range ticker.C {
 			// log.Debugf("<- ping")
-			notificationListener.conn.WriteMessage(websocket.TextMessage, []byte("ping"))
+			err := notificationListener.conn.WriteMessage(websocket.TextMessage, []byte("ping"))
+			if err != nil {
+				log.Errorf("Error when pinging websocket. %s", err)
+			}
 		}
 	}()
 
@@ -69,9 +78,9 @@ func startNotificationListener(wallet *RPCWallet) (*NotificationListener, error)
 		for {
 			_, message, err := conn.ReadMessage()
 			if err != nil {
-				if websocket.IsCloseError(err) {
+				if websocket.IsCloseError(err) || websocket.IsUnexpectedCloseError(err) {
 					log.Infof("Reconnecting to %s", u.String())
-					conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+					conn, _, err := dialerWithCookies.Dial(u.String(), nil)
 					if err != nil {
 						log.Error(err)
 						return
@@ -83,57 +92,65 @@ func startNotificationListener(wallet *RPCWallet) (*NotificationListener, error)
 				}
 			}
 
-			if string(message) == "pong" {
+			if string(message) == "pong" || string(message) == "ping" || string(message) == "success" {
 				continue
 			}
 
-			//log.Debugf("-> %s", message)
+			if strings.HasPrefix(string(message), "error") {
+				log.Warningf("Websocket returned - %s", string(message))
+				continue
+			}
+
+			log.Debugf("-> %s", message)
 
 			var getTx btcjson.GetTransactionResult
 			err = json.Unmarshal(message, &getTx)
 
 			if err == nil {
-				txBytes, err := hex.DecodeString(getTx.Hex)
+				err = ingestTransaction(wallet, getTx)
 				if err != nil {
-					log.Error(err)
-					continue
-				}
-
-				transaction := wire.NewMsgTx(1)
-				transaction.BtcDecode(bytes.NewReader(txBytes), 1, wire.BaseEncoding)
-
-				var blockHeight int32
-
-				if getTx.BlockHash != "" {
-					blockhash, err := chainhash.NewHashFromStr(getTx.BlockHash)
-					if err != nil {
-						log.Error(err)
-						continue
-					}
-
-					block, err := wallet.rpcClient.GetBlockVerbose(blockhash)
-					if err != nil {
-						log.Error(err)
-						continue
-					}
-					blockHeight = int32(block.Height)
-				}
-
-				hits, err := wallet.txstore.Ingest(transaction, blockHeight, time.Unix(getTx.BlockTime, 0))
-				if err != nil {
-					log.Errorf("Error ingesting tx: %s\n", err.Error())
-					continue
-				}
-				if hits == 0 {
-					log.Debugf("Tx %s from Peer%d had no hits, filter false positive.", transaction.TxHash().String())
 					continue
 				}
 				notificationListener.updateFilterAndSend()
-				log.Infof("Tx %s ingested", transaction.TxHash().String())
 			} else {
 				log.Errorf("msg: %s, err: %s", string(message), err)
 			}
 		}
 	}()
 	return notificationListener, nil
+}
+
+func ingestTransaction(wallet *RPCWallet, getTx btcjson.GetTransactionResult) error {
+	txBytes, err := hex.DecodeString(getTx.Hex)
+	if err != nil {
+		log.Error(err)
+	}
+
+	transaction := wire.NewMsgTx(1)
+	transaction.BtcDecode(bytes.NewReader(txBytes), 1, wire.BaseEncoding)
+
+	var blockHeight int32
+
+	if getTx.BlockHash != "" {
+		blockhash, err := chainhash.NewHashFromStr(getTx.BlockHash)
+		if err != nil {
+			log.Error(err)
+		}
+
+		block, err := wallet.rpcClient.GetBlockVerbose(blockhash)
+		if err != nil {
+			log.Error(err)
+		}
+		blockHeight = int32(block.Height)
+	}
+
+	hits, err := wallet.txstore.Ingest(transaction, blockHeight, time.Unix(getTx.BlockTime, 0))
+	if err != nil {
+		log.Errorf("Error ingesting tx: %s\n", err.Error())
+	}
+	if hits == 0 {
+		log.Debugf("Tx %s from Peer%d had no hits, filter false positive.", transaction.TxHash().String())
+	}
+	log.Infof("Tx %s ingested", transaction.TxHash().String())
+	return nil
 }
