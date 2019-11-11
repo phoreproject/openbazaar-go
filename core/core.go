60d12120d3a43e375a8d2af85dd7f3e59dd16bfd
anchaj <anchaj@users.noreply.github.com>
2019-11-12 00:07:59 +0100

    Bump server version

--- core/core.go
+++ core/core.go
@@ -34,7 +34,7 @@ import (
 
 const (
 	// VERSION - current version
-	VERSION = "2.2.1-dev"
+	VERSION = "2.2.1-dev3"
 	// USERAGENT - user-agent header string
 	USERAGENT = "/Phore-Marketplace-go:" + VERSION + "/"
 )
